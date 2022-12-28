01baadfc4f82431f07558efc19f0a490f1da5e47
Yury Lobkov <racoon.enot@gmail.com>
2022-12-28 01:18:46 -0300

    drink list page html shows unordered list

--- drinks/admin.py
+++ drinks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Cocktail
 # Register your models here.
+
+class CocktailAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Cocktail, CocktailAdmin)
--- drinks/models.py
+++ drinks/models.py
@@ -6,3 +6,6 @@ class Cocktail(models.Model):
     group = models.CharField(max_length = 30)
     volume = models.SmallIntegerField()
 
+    def __str__(self):
+        return f'{self.name},{self.group},{self.volume}%'
+
