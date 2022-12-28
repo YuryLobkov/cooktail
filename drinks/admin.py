428ef3e6c3ae0d00d9e0cc63f16c6cbdae14c99c
Yury Lobkov <racoon.enot@gmail.com>
2022-12-28 17:38:14 -0300

    foreign key for cocktail_group

--- drinks/admin.py
+++ drinks/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Cocktail
+from .models import Cocktail, GroupsCocktail
 # Register your models here.
 
 class CocktailAdmin(admin.ModelAdmin):
     pass
-
+class CocktailGroupAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Cocktail, CocktailAdmin)
+admin.site.register(GroupsCocktail, CocktailGroupAdmin)
--- drinks/models.py
+++ drinks/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GroupsCocktail(models.Model):
+    group_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.group_name}'
+
+
 class Cocktail(models.Model):
     name = models.CharField(max_length=30)
-    group = models.CharField(max_length = 30)
+    group = models.ForeignKey('GroupsCocktail', on_delete=models.CASCADE)
     volume = models.SmallIntegerField()
 
     def __str__(self):
