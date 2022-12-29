9c9c6e112bdf4bda70ad013d4a77fc8e4351eef9
Yury Lobkov <racoon.enot@gmail.com>
2022-12-29 01:42:04 -0300

    add admin view for inventory model

--- drinks/admin.py
+++ drinks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cocktail, GroupsCocktail, Ingredients
+from .models import Cocktail, GroupsCocktail, Ingredients, Inventory
 # Register your models here.
 
 class CocktailAdmin(admin.ModelAdmin):
@@ -8,7 +8,10 @@ class CocktailGroupAdmin(admin.ModelAdmin):
     pass
 class IngredientsAdmin(admin.ModelAdmin):
     pass
+class InventoryAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Cocktail, CocktailAdmin)
 admin.site.register(GroupsCocktail, CocktailGroupAdmin)
 admin.site.register(Ingredients, IngredientsAdmin)
+admin.site.register(Inventory, InventoryAdmin)
