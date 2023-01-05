201382e7e1866177cd406d7f63c0453194fe0fa3
Yury Lobkov <racoon.enot@gmail.com>
2023-01-05 06:42:12 -0300

    images now shown in detail view

--- cooktail/settings.py
+++ cooktail/settings.py
@@ -133,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media contetn storage
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+#MEDIA_URL = os.path.join(BASE_DIR, 'media/').replace(os.sep,'/')
