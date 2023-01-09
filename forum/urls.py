3ea5ed2bef4520805ee47378d46faa54a1e50b2d
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-01-09 22:30:06 +0500

    Add a blog

---
+++ forum/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from .views import UpdateView
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('sign-up', views.sign_up, name='sign-up'),
+    path('posts', views.post_list, name='post_list'),
+    path('create-post', views.create_post, name='create-post'),
+]
