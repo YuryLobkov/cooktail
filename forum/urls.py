3eedcd1e362fe0181086ea0a7cf788b4c60019f8
Yury Lobkov <racoon.enot@gmail.com>
2023-01-10 12:43:02 -0300

    removed double home url

--- forum/urls.py
+++ forum/urls.py
@@ -6,7 +6,7 @@ app_name = 'forum'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home', views.home, name='home'),
+    # path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign-up'),
     path('posts/', views.post_list, name='post_list'),
     path('create-post', views.create_post, name='create-post'),
