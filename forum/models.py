6413fe3e3a246907afaa34b17c360f8e21243ca5
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-01-10 21:22:37 +0500

    added a comment model and made migrations

--- forum/models.py
+++ forum/models.py
@@ -10,3 +10,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + '\n' + self.content
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
