6406ab5f0687e8bb4b42ba181f785dec7946e264
Yury Lobkov <racoon.enot@gmail.com>
2023-01-18 10:52:26 -0300

    add post page layout

--- forum/forms.py
+++ forum/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 
 class RegistrationForm(UserCreationForm):
     model = User
@@ -11,3 +13,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+    helper = FormHelper()
+    helper.form_class = 'form-group'
+    helper.layout = Layout(
+        Field('title', css_class='form-control custom-form-field'),
+        Field('content', css_class='form-control custom-form-field')
+    )
+    helper.add_input(Submit('submit', 'Submit', css_class='btn-success'))
