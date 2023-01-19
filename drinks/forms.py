d015483bc0732a50dd6d9ad9092fc3dab0051e05
Yury Lobkov <racoon.enot@gmail.com>
2023-01-19 13:17:39 -0300

    form userstorage

--- drinks/forms.py
+++ drinks/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from models import UserStorage
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
+
+
+class StorageForm(forms.ModelForm):
+    class Meta:
+        model = UserStorage
+        fields = ['user_ingredients']
+    helper = FormHelper()
+    helper.form_class = 'form-group'
+    helper.layout = Layout(Field('body',css_class='form-control mt-2 mb-3'))
