9de7429c37840c96245ad59a62e54ece1af9af6a
Yury Lobkov <racoon.enot@gmail.com>
2023-01-24 13:43:26 -0300

    queryset to user ingredients field

--- drinks/forms.py
+++ drinks/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import UserStorage
+from .models import UserStorage, Ingredients
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field
 
@@ -11,3 +11,7 @@ class StorageForm(forms.ModelForm):
     helper = FormHelper()
     helper.form_class = 'form-group'
     helper.layout = Layout(Field('body',css_class='form-control mt-2 mb-3'))
+
+    def __init__(self,*args,**kwargs):
+        super (StorageForm,self ).__init__(*args,**kwargs)
+        self.fields['user_ingredients'].queryset = Ingredients.objects.filter(name = 'Vodka')
