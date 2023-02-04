5daad3f3273ebb8cd967fc4e62b06e61170d722d
Yury Lobkov <racoon.enot@gmail.com>
2023-02-04 11:19:34 -0300

    allowed_hosts and admin_url

--- cooktail/settings.py
+++ cooktail/settings.py
@@ -31,7 +31,7 @@ CSRF_COOKIE_SECURE = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'RaccoonEnot.pythonanywhere.com']
 
 
 # Application definition
